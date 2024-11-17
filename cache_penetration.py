fb8ee37c99eb6cdca5d6e91a7f34c9338040c5f4
TaimaraColon <taimara.colon@upr.edu>
2024-11-17 00:42:45 -0400

    some changes

--- cache_penetration.py
+++ cache_penetration.py
@@ -1,6 +1,4 @@
 import sys
-import mmh3
-import hashlib
 import array
 import csv
 import math
@@ -33,78 +31,62 @@ def setBit(array_name, bit_num):
     array_name[record] |= mask
     return(array_name[record])
 
-# clearBit() returns an integer with the bit at 'bit_num' cleared.
-def clearBit(array_name, bit_num):
-    record = bit_num >> 5
-    offset = bit_num & 31
-    mask = ~(1 << offset)
-    array_name[record] &= mask
-    return(array_name[record])
-
-# toggleBit() returns an integer with the bit at 'bit_num' inverted, 0 -> 1 and 1 -> 0.
-def toggleBit(array_name, bit_num):
-    record = bit_num >> 5
-    offset = bit_num & 31
-    mask = 1 << offset
-    array_name[record] ^= mask
-    return(array_name[record])
-
 def hashing(data, bitArraySize, k):
-    # salted_data = f"{data}{seed}".encode()
-    # hash_object = hashlib.sha256(salted_data)
-    # hash_value = int(hash_object.hexdigest(), 16)
-    # return hash_value % bitArraySize
-    hash_value = mmh3.hash(data, k)
-    return hash_value % bitArraySize
+    hashValue = hash(f"{data}{k}")
+    return hashValue % bitArraySize
 
-def placeElementInBitArray(bitArray, element, bitArraySize, k):
-    for function in range(k):
-        index = hashing(element, bitArraySize, function)
+def addToBloomFilter(bitArray, element, bitArraySize, k):
+    for variationOfHash in range(k):
+        index = hashing(element, bitArraySize, variationOfHash)
         setBit(bitArray, index)
-    #FOR TESTING
-    #print(f"Email: '{element}' ; index: {index}")
-
-def BloomFilter(emailToCheck,bitArray, bitArraySize, k):
-    for function in range(k):
-        index = hashing(emailToCheck, bitArraySize, function)
-        if not testBit(bitArray, index):
-            print(f"{emailToCheck},Not in the DB")
-            return
-    print(f"{emailToCheck},Probably in the DB")
-            
 
 def BloomFilterCalculator(n, p):
     m = math.ceil((n * math.log(p)) / math.log(1 / math.pow(2, math.log(2))))
     k = round((m / n) * math.log(2))
     return m, k
-    
-if len(sys.argv) > 1:
-    filename1 = sys.argv[1]
-    filename2 = sys.argv[2]
-    
-    with open(filename1, "r") as file:
+   
+def checkBloomFilter(emailToCheck,bitArray, bitArraySize, k):
+    for variationOfHash in range(k):
+        index = hashing(emailToCheck, bitArraySize, variationOfHash)
+        if testBit(bitArray, index) == 0: #if 0 not in DB
+            print(f"{emailToCheck},Not in the DB")
+            return
+    print(f"{emailToCheck},Probably in the DB")
+
+def readEmailsFromFile(filename):
+    """
+    Reads emails from csv file.
+
+    Args:
+        arg1 (str): Name of csv file containing emails.
+        
+    Returns:
+        list: A list of emails extracted from the csv file.
+    """
+    with open(filename, "r") as file:
         reader = csv.reader(file)
         next(reader)
         emails = []
         for row in reader:
             for email in row:
                 emails.append(email)
-        m, k = BloomFilterCalculator(len(emails),0.0000001)
-        bloomArray  = makeBitArray(m) #Creates an m-bit array
-        for email in emails:
-            placeElementInBitArray(bloomArray, email, m, k)
+    return emails
+
+                    
+if len(sys.argv) > 1:
+    filename1 = sys.argv[1]
+    filename2 = sys.argv[2]
     
-    with open(filename2, "r") as file:
-        reader = csv.reader(file)
-        next(reader)
-        emailsToCompare = []
-        for row in reader:
-            for email in row:
-                emailsToCompare.append(email)
-        for email in emailsToCompare:
-            BloomFilter(email, bloomArray, m, k)
-
+    emails = readEmailsFromFile(filename1)
     
+    m, k = BloomFilterCalculator(len(emails), 0.0000001)
 
-        
+    bloomArray  = makeBitArray(m) #Creates an m-bit array
 
+    for email in emails:
+        addToBloomFilter(bloomArray, email, m, k)
+    
+    emailsToCompare = readEmailsFromFile(filename2)
+
+    for email in emailsToCompare:
+        checkBloomFilter(email, bloomArray, m, k)
